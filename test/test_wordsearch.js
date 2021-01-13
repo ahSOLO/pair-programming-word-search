d8312dceb5a635bdf9714542f4bacc61a4416148
Andrew Han <junmenghan@gmail.com>
2021-01-13 20:53:13 +0000

    Finalized code

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -89,4 +89,5 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
 });
--- wordsearch.js
+++ wordsearch.js
@@ -28,14 +28,7 @@ const wordSearch = (letters, word) => {
     for (l of verticalReverseJoin) {
         if (l.includes(word)) return true
     }
-    // 6 x 5
-    // O X X X X X 
-    // O O X X X X
-    // O O O X X X 
-    // O O O O X X
-    // O O O O O X 
-
-    
+  
     return false;
 }
 
